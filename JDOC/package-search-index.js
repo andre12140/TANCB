e8bb4d47f85eebe452b539b427c25cd6d217db44
André Barbosa <48558708+andre12140@users.noreply.github.com>
2021-12-05 13:21:16 +0000

    Add files via upload

---
+++ JDOC/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"classifier"},{"l":"graph"},{"l":"main"},{"l":"metrics"},{"l":"score"},{"l":"spanningTree"},{"l":"tree"}]
